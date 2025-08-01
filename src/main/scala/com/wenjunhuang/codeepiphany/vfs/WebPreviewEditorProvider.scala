77a1119f6625749e0bc266354ad73218c4f711a7
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2025-08-01 12:16:05 +0800

    fix(vfs): 为 WebPreviewEditorProvider 实现 DumbAware 接口

--- src/main/scala/com/wenjunhuang/codeepiphany/vfs/WebPreviewEditorProvider.scala
+++ src/main/scala/com/wenjunhuang/codeepiphany/vfs/WebPreviewEditorProvider.scala
@@ -1,12 +1,12 @@
 package com.wenjunhuang.codeepiphany.vfs
 
-import com.intellij.openapi.fileEditor.{FileEditor, FileEditorPolicy}
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.fileEditor.{ FileEditor, FileEditorPolicy }
+import com.intellij.openapi.project.{ DumbAware, Project }
 import com.intellij.openapi.vfs.VirtualFile
 
 import com.wenjunhuang.codeepiphany.utils.walkaround.FileEditorProviderBridge
 
-class WebPreviewEditorProvider extends FileEditorProviderBridge {
+class WebPreviewEditorProvider extends FileEditorProviderBridge with DumbAware {
 
   override def accept(project: Project, file: VirtualFile): Boolean = file.isInstanceOf[WebPreviewVirtualFile]
 
