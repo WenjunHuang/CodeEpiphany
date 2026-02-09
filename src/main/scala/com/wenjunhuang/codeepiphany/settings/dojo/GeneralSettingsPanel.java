0cf2afa58f0cf8cc93c84a5bd3af8e7762563843
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2026-02-09 21:58:15 +0800

    refactor(settings): simplify add language label and adjust UI layout

--- src/main/scala/com/wenjunhuang/codeepiphany/settings/dojo/GeneralSettingsPanel.java
+++ src/main/scala/com/wenjunhuang/codeepiphany/settings/dojo/GeneralSettingsPanel.java
@@ -44,9 +44,6 @@ public class GeneralSettingsPanel extends SettingsUi<BaseCodeDojoSettings.CodeDo
             editor.setBorder(new DarculaEditorTextFieldBorder(myCSSEditor, editor));
             editor.setHighlighter(ChallengeFileTemplateHighlighter.createLanguageEditorHighlighterByExtension(myProject, Option.apply("css")));
         });
-        myHowToLabel = new JBLabel();
-        myHowToLabel.setCopyable(false);
-        myHowToLabel.setAllowAutoWrapping(true);
     }
 
     {
@@ -67,14 +64,15 @@ public class GeneralSettingsPanel extends SettingsUi<BaseCodeDojoSettings.CodeDo
         createUIComponents();
         myRoot = new JPanel();
         myRoot.setLayout(new GridLayoutManager(4, 2, new Insets(0, 0, 0, 0), -1, -1));
-        this.$$$loadLabelText$$$(myHowToLabel, this.$$$getMessageFromBundle$$$("messages/PluginBundle", "configure.addLanguage.label"));
-        myRoot.add(myHowToLabel, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
         this.$$$loadLabelText$$$(label1, this.$$$getMessageFromBundle$$$("messages/PluginBundle", "configure.general.descriptionCSS"));
         myRoot.add(label1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         myRoot.add(myCSSEditor, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, new Dimension(-1, 300), new Dimension(-1, 300), new Dimension(-1, 300), 0, false));
         final Spacer spacer1 = new Spacer();
         myRoot.add(spacer1, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        myHowToLabel = new JBLabel();
+        this.$$$loadLabelText$$$(myHowToLabel, this.$$$getMessageFromBundle$$$("messages/PluginBundle", "configure.addLanguage.label"));
+        myRoot.add(myHowToLabel, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     private static Method $$$cachedGetBundleMethod$$$ = null;
