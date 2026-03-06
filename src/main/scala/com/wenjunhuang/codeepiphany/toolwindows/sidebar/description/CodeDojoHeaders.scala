fe005e35a4a177c2ed7c0849165e5f879a2f888d
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2026-03-06 10:29:22 +0800

    refactor(CodeDojoHeaders): 移除多余空行并优化HTML脚本格式
    移除CodeDojoHeaders.scala中的多余空行，并调整HTML脚本的格式以提高代码可读性。主要变更包括删除重复的空行和优化AtCoder头部脚本的缩进。

--- src/main/scala/com/wenjunhuang/codeepiphany/toolwindows/sidebar/description/CodeDojoHeaders.scala
+++ src/main/scala/com/wenjunhuang/codeepiphany/toolwindows/sidebar/description/CodeDojoHeaders.scala
@@ -14,6 +14,7 @@ object CodeDojoHeaders {
   private def getCodeForcesHeader: String =
     // language=HTML
     """
+      |<script type="text/javascript" src="https://codeforces.com/mathjax.codeforces.org/MathJax.js?config=TeX-AMS_HTML-full"></script>
       |<script type="text/javascript">
       |    window.MathJax = {
       |      tex2jax: {
@@ -22,28 +23,25 @@ object CodeDojoHeaders {
       |      }
       |    };
       |</script>
-      |<script type="text/javascript" src="https://codeforces.com/mathjax.codeforces.org/MathJax.js?config=TeX-AMS_HTML-full"></script>
       |""".stripMargin
 
   private def getAtCoderHeader: String =
     // language=HTML
     """
-      |<script src="atcoder/jquery-1.9.1.min.js"></script>
-      |<script>$(function(){$('var').each(function(){var html=$(this).html().replace(/<sub>/g,'_{').replace(/<\/sub>/g,'}');$(this).html('\\('+html+'\\)');});});</script>
+      |<script type="text/javascript" src="atcoder/jquery-1.9.1.min.js"></script>
       |<script>
-      |			var katexOptions = {
-      |				delimiters: [
-      |					{left: "$$", right: "$$", display: true},
-      |					{left: "\\(", right: "\\)", display: false},
-      |					{left: "\\[", right: "\\]", display: true}
-      |				],
-      |      	ignoredTags: ["script", "noscript", "style", "textarea", "code", "option"],
-      |				ignoredClasses: ["prettyprint", "source-code-for-copy"],
-      |				throwOnError: false
-      |			};
-      |			document.addEventListener("DOMContentLoaded", function() { renderMathInElement(document.body, katexOptions);});
-      |</script>
-      |<script>
+      |$(function(){$('var').each(function(){var html=$(this).html().replace(/<sub>/g,'_{').replace(/<\/sub>/g,'}');$(this).html('\\('+html+'\\)');});});
+      |var katexOptions = {
+      |	delimiters: [
+      |		{left: "$$", right: "$$", display: true},
+      |		{left: "\\(", right: "\\)", display: false},
+      |		{left: "\\[", right: "\\]", display: true}
+      |	],
+      | ignoredTags: ["script", "noscript", "style", "textarea", "code", "option"],
+      |	ignoredClasses: ["prettyprint", "source-code-for-copy"],
+      |	throwOnError: false
+      |};
+      |document.addEventListener("DOMContentLoaded", function() { renderMathInElement(document.body, katexOptions);});
       |var LANG = "en";
       |// local storage
       |function setLS(key, val) {
@@ -67,20 +65,20 @@ object CodeDojoHeaders {
     // language=html
     """
     |<script>
-    |    document.addEventListener("DOMContentLoaded", function() {
-    |        renderMathInElement(document.body, {
-    |          // customised options
-    |          // • auto-render specific keys, e.g.:
-    |          delimiters: [
-    |              {left: '$$', right: '$$', display: true},
-    |              {left: '$', right: '$', display: false},
-    |              {left: '\\(', right: '\\)', display: false},
-    |              {left: '\\[', right: '\\]', display: true}
-    |          ],
-    |          // • rendering keys, e.g.:
-    |          throwOnError : false
-    |        });
+    |document.addEventListener("DOMContentLoaded", function() {
+    |    renderMathInElement(document.body, {
+    |      // customised options
+    |      // • auto-render specific keys, e.g.:
+    |      delimiters: [
+    |          {left: '$$', right: '$$', display: true},
+    |          {left: '$', right: '$', display: false},
+    |          {left: '\\(', right: '\\)', display: false},
+    |          {left: '\\[', right: '\\]', display: true}
+    |      ],
+    |      // • rendering keys, e.g.:
+    |      throwOnError : false
     |    });
+    |});
     |</script>
     |""".stripMargin
 }
